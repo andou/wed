26738a1e3ea45953d475ef9811c7608b3b3a1a23
andou <antonio.pastorino@gmail.com>
2016-02-03 20:37:17 +0100

    TYpo

--- wp-content/themes/wedding/page-templates/home.php
+++ wp-content/themes/wedding/page-templates/home.php
@@ -115,7 +115,7 @@ $counter = 0;
       </div>
       <div class="12u$">
         <ul class="actions">
-          <li><a href="#first" class="button style2 scrolly bottom">Torna in cima</a></li>
+          <li><a href="#first" class="button style1 scrolly bottom">Torna in cima</a></li>
         </ul>
       </div>
       <?php wp_nonce_field('submit_participation', '_sp'); ?>
