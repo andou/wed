455797dbfd9c1686020c226350e3a526de2c10d7
andou <antonio.pastorino@gmail.com>
2016-05-12 12:46:23 +0200

    Viewport

--- wp-content/themes/wedding/header.php
+++ wp-content/themes/wedding/header.php
@@ -1,18 +1,16 @@
 <?php
 /**
- * The template for displaying the header
+ * The template for displaying the header.
  *
  * Displays all of the head element and everything up until the "site-content" div.
  *
- * @package WordPress
- * @subpackage Twenty_Fifteen
  * @since Twenty Fifteen 1.0
  */
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?> class="no-js">
   <head>
     <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
     <link rel="profile" href="http://gmpg.org/xfn/11">
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -48,7 +46,3 @@
 
   <body <?php body_class(); ?>>
     <div id="page" class="hfeed site">
-
-
-
-
